b34f6357f4b257eec56e67bab30b7e1b26b048cc
gitaa001 <18223122@std.stei.itb.ac.id>
2025-05-28 17:43:36 +0700

    FEAT: edit worldmap menu

--- src/gui/WorldMapPanel.java
+++ src/gui/WorldMapPanel.java
@@ -1,10 +1,17 @@
 package src.gui;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import src.map.*;
+import src.gui.GamePanel;
+import src.map.Location;
+import src.map.Point;
 
 public class WorldMapPanel extends JPanel implements ActionListener {
     private JButton forrestRiverButton;
@@ -18,106 +25,116 @@ public class WorldMapPanel extends JPanel implements ActionListener {
     public WorldMapPanel(GamePanel gp) {
         this.gp = gp;
         this.setLayout(null);
-        this.setPreferredSize(new Dimension(800, 600));
+        this.setPreferredSize(new Dimension(800, 600)); 
 
-        ImageIcon icon = new ImageIcon("res/worldmap/huahauhau.png");
+        // Tambahkan tombol
+        forrestRiverButton = createImageButton("Forest River", 
+            "res/worldmap/forrestRiverButton.png", 
+            "res/worldmap/forrestRiverButton_hover.png", 
+            220, 100);
+        mountainLakeButton = createImageButton("Mountain Lake", 
+            "res/worldmap/mountainLakeButton.png", 
+            "res/worldmap/mountainLakeButton_hover.png", 
+            440, 100);
+        oceanButton = createImageButton("Ocean", 
+            "res/worldmap/oceanButton.png", 
+            "res/worldmap/oceanButton_hover.png", 
+            220, 250);
+        storeButton = createImageButton("Store", 
+            "res/worldmap/storeButton.png", 
+            "res/worldmap/storeButton_hover.png", 
+            440, 250);
+        farmButton = createImageButton("Farm", 
+            "res/worldmap/farmButton.png", 
+            "res/worldmap/farmButton_hover.png", 
+            220, 400);
+        npcHouseButton = createImageButton("NPC House", 
+            "res/worldmap/npcHouseButton.png", 
+            "res/worldmap/npcHouseButton_hover.png", 
+            440, 400);
 
-        // Atas
-        forrestRiverButton = createButton(icon, 200, 100);
-        mountainLakeButton = createButton(icon, 400, 100);
-        // Tengah
-        oceanButton = createButton(icon, 200, 250);
-        storeButton = createButton(icon, 400, 250); 
-        // Bawah
-        farmButton = createButton(icon, 200, 400); 
-        npcHouseButton = createButton(icon, 400, 400); 
+        // Tambah ke panel
+        add(forrestRiverButton);
+        add(mountainLakeButton);
+        add(oceanButton);
+        add(storeButton);
+        add(farmButton);
+        add(npcHouseButton);
 
-        // Background label
+        // Background
+        ImageIcon papan = new ImageIcon("res/worldmap/papan.png");
+        JLabel papanLabel = new JLabel(papan);
+        papanLabel.setBounds(160, 60, 500, 500);
+        add(papanLabel);
+
+        // Background
         ImageIcon background = new ImageIcon("res/worldmap/bg-map.png");
         JLabel bgLabel = new JLabel(background);
         bgLabel.setBounds(0, 0, 800, 600);
-
-        // Add buttons first (so they are on top of background)
-        this.add(forrestRiverButton);
-        this.add(mountainLakeButton);
-        this.add(oceanButton);
-        this.add(storeButton);
-        this.add(farmButton);
-        this.add(npcHouseButton);
-        this.add(bgLabel);
+        add(bgLabel);
     }
 
-    private JButton createButton(ImageIcon icon, int x, int y) {
+    private JButton createImageButton(String name, String iconPath, String hoverPath, int x, int y) {
+        ImageIcon icon = new ImageIcon(iconPath);
+        ImageIcon hoverIcon = new ImageIcon(hoverPath);
+
         JButton button = new JButton(icon);
-        button.setBounds(x, y, 100, 70);
+        button.setBounds(x, y, 160, 130);
+        button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.BOTTOM);
+        button.setFont(new Font("Arial", Font.BOLD, 14));
+        button.setForeground(Color.WHITE);
         button.setBorderPainted(false);
         button.setContentAreaFilled(false);
         button.setFocusPainted(false);
         button.setOpaque(false);
+        button.setRolloverIcon(hoverIcon);
+        button.setToolTipText(name); // optional tooltip
         button.addActionListener(this);
+
+        // Tambahkan label teks overlay
+        JLabel label = new JLabel(name, JLabel.CENTER);
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("Arial", Font.BOLD, 14));
+        label.setBackground(new Color(0, 0, 0, 150));
+        label.setOpaque(true);
+        label.setBounds(0, 100, 160, 30); // posisinya di bawah gambar
+        button.setLayout(null);
+        button.add(label);
+
         return button;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         String location = gp.player.getPlayerLocation().getName();
+
         if (e.getSource() == forrestRiverButton) {
-            if (location.equals("ForestRiver")) {
-                gp.mainPanel.showGame();
-                gp.resetPlayerMovement();
-                gp.player.setPlayerLocation(new Location("ForestRiver", new Point(gp.player.getPlayerLocation().getCurrentPoint().getX()-1, gp.player.getPlayerLocation().getCurrentPoint().getY()-1)));
-            }
-            else{
-                gp.goToForestRiver();
-                gp.resetPlayerMovement();
-            }
-        } 
-        else if (e.getSource() == mountainLakeButton) {
-            if (location.equals("MountainLake")) {
-                gp.mainPanel.showGame();
-                gp.resetPlayerMovement();
-                gp.player.setPlayerLocation(new Location("MountainLake", new Point(gp.player.getPlayerLocation().getCurrentPoint().getX()-1, gp.player.getPlayerLocation().getCurrentPoint().getY()-1)));
-            }
-            else{
-                gp.goToMountainLake();
-                gp.resetPlayerMovement();
-            }
-        } 
-        else if (e.getSource() == oceanButton) {
-            if (location.equals("Ocean")) {
-                gp.mainPanel.showGame();
-                gp.resetPlayerMovement();
-                gp.player.setPlayerLocation(new Location("Ocean", new Point(gp.player.getPlayerLocation().getCurrentPoint().getX()-1, gp.player.getPlayerLocation().getCurrentPoint().getY()-1)));
-            }
-            else{
-                gp.goToOcean();
-                gp.resetPlayerMovement();
-            }
-        } 
-        else if (e.getSource() == storeButton) {
-            if (location.equals("Store")) {
-                gp.mainPanel.showGame();
-                gp.resetPlayerMovement();
-                gp.player.setPlayerLocation(new Location("Store", new Point(gp.player.getPlayerLocation().getCurrentPoint().getX()-1, gp.player.getPlayerLocation().getCurrentPoint().getY()-1)));
-            }
-            else{
-                gp.goToStore();
-                gp.resetPlayerMovement();
-            }
-        } 
-        else if (e.getSource() == farmButton) {
-            if (location.equals("Farm")) {
-                gp.mainPanel.showGame();
-                gp.resetPlayerMovement();
-                gp.player.setPlayerLocation(new Location("Farm", new Point(gp.player.getPlayerLocation().getCurrentPoint().getX()-1, gp.player.getPlayerLocation().getCurrentPoint().getY()-1)));
-            } else {
-                gp.returnToFarm();
-                gp.resetPlayerMovement();
-            }
-        } 
-        else if (e.getSource() == npcHouseButton) {
-            System.out.println("Button 6 clicked");
+            goToLocation("ForestRiver", () -> gp.goToForestRiver());
+        } else if (e.getSource() == mountainLakeButton) {
+            goToLocation("MountainLake", () -> gp.goToMountainLake());
+        } else if (e.getSource() == oceanButton) {
+            goToLocation("Ocean", () -> gp.goToOcean());
+        } else if (e.getSource() == storeButton) {
+            goToLocation("Store", () -> gp.goToStore());
+        } else if (e.getSource() == farmButton) {
+            goToLocation("Farm", () -> gp.returnToFarm());
+        } else if (e.getSource() == npcHouseButton) {
+            System.out.println("Button NPC clicked");
         }
     }
 
+    private void goToLocation(String name, Runnable changeLocationAction) {
+        String current = gp.player.getPlayerLocation().getName();
+        if (current.equals(name)) {
+            gp.mainPanel.showGame();
+            gp.resetPlayerMovement();
+            gp.player.setPlayerLocation(new Location(name,
+                new Point(gp.player.getPlayerLocation().getCurrentPoint().getX() - 1,
+                          gp.player.getPlayerLocation().getCurrentPoint().getY() - 1)));
+        } else {
+            changeLocationAction.run();
+            gp.resetPlayerMovement();
+        }
+    }
 }
