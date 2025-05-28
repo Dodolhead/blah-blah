6e87d421740aea8dd198daeb095498bdbfcbcbcb
Nicholas Zefanya <87974728+nicholaszef@users.noreply.github.com>
2025-05-28 14:13:10 +0700

    fix runmac.sh

--- runMac.sh
+++ runMac.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 echo "[1] Deleting old class files..."
-rm -rf out/*.class
+if [ -d "out" ]; then
+    rm -rf out
+    echo "Deleted previous 'out' directory."
+else
+    echo "'out' directory not found, skipping delete."
+fi
 
 echo "[2] Compiling Java files..."
+mkdir -p out
 javac -d out \
 src/endgame/*.java \
 src/gui/*.java \
